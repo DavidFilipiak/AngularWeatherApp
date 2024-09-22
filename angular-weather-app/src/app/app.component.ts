4d239c4b5a1f1b40972bdd13441f67680269b586
DavidFilipiak <davidd.filipiak@gmail.com>
2024-09-22 20:07:03 +0200

    added countries and improved the header

--- angular-weather-app/src/app/app.component.ts
+++ angular-weather-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { WeatherService } from './services/weather.service';
-import { Observable } from 'rxjs';
+import * as countries_json from '../assets/countries.json';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,26 +11,51 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   public citySearch: string = '';
   public city: any;
+  public countries = (countries_json as any).default;
 
   constructor(
-    private weatherService: WeatherService
+    private weatherService: WeatherService,
+    private title: Title,
   ) { }
 
   ngOnInit() {
-
+    this.title.setTitle('World Weather');
   }
 
   searchCity() {
+    if (!this.citySearch) {
+      alert('Please enter a city name');
+      return;
+    }
     this.weatherService.getWeatherForCity(this.citySearch).subscribe({
       next: (data: any) => {
-        console.log(data);
         data.weather[0].iconUrl = this.weatherService.getWeatherIcon(data.weather[0].icon);
+        data.countryName = this.getCountryName(data.sys.country);
+        data.time = this.getCountryTime(data.timezone);
         this.city = data;
       },
       error: (error) => {
-        alert(error.error.message);
+        if (error.status == 404) {
+          alert('City not found');
+        }
+        else {
+          alert('An unknown error occurred.');
+        }
       }
     });
+  }
+
+  getCountryName(code: string) {
+    const country = this.countries.find((country: any) => country.code === code);
+    return country ? country.name : code;
+  }
+
+  getCountryTime(timezone: number) {
+    const timeNow = new Date();
+    const time = new Date(timeNow.getTime() + (timeNow.getTimezoneOffset() * 60 * 1000) + (timezone * 1000));
+    const timeString = time.toLocaleTimeString();
+    const dateString = time.toLocaleDateString();
+    return timeString + ', ' + dateString;
   }
 
 }
