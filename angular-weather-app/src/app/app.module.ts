645d2dbb0c904bef5e2dbb86ead9dfc6eb886560
DavidFilipiak <davidd.filipiak@gmail.com>
2024-09-22 14:23:16 +0200

    added styling and a weather-card component

--- angular-weather-app/src/app/app.module.ts
+++ angular-weather-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { WeatherCardComponent } from "./weather-card/weather-card.component";
 
 @NgModule({
   declarations: [
@@ -12,8 +13,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    WeatherCardComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ angular-weather-app/src/app/weather-card/weather-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-card.component.html',
+  styleUrl: './weather-card.component.css'
+})
+export class WeatherCardComponent {
+  @Input() city: any;
+}
